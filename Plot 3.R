1946f05fe6707f82406c1f35312161b79dc3f881
Ng Bo Lin <ngbolin91@gmail.com>
2017-02-26 17:16:05 +0800

    Update Plot 3.R

--- Plot 3.R
+++ Plot 3.R
@@ -13,11 +13,15 @@ df <- hpcons[(hpcons$Date == "1/2/2007" | hpcons$Date == "2/2/2007"), ]
 df$DateTime <- strptime(paste(df$Date, df$Time, sep = " "), "%d/%m/%Y %H:%M:%S") 
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 
-# Converting Date to Weekday
+# Plotting 3 Lines
 png(file = "Plot 3.png")
 plot(df$DateTime, df$Sub_metering_1, type = "l", xaxt = "n", xlab = "", ylab = "Energy sub metering")
 lines(df$DateTime, df$Sub_metering_2, col = "red")
 lines(df$DateTime, df$Sub_metering_3, col = "blue")
+
+# Ensure X-axis reflects Day of Week
 axis.POSIXct(1, df$DateTime, format = "%a")
+
+# Plotting Legend
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "blue", "red"), cex = 0.5)
 dev.off()
